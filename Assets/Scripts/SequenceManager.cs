50a7a4ae9e42b35e0cebfee72e4049d38fec189d
reiya2200 <t.reiya.2200@gmail.com>
2021-08-17 17:36:22 +0900

    Add SceneManager & Loader

---
+++ Assets/Scripts/SequenceManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SequenceManager : MonoBehaviour
+{
+    [SerializeField]
+    private string _initialSceneName = "Title";
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        SceneManager.LoadScene(_initialSceneName);
+    }
+
+    void Update()
+    {
+    }
+
+    public void GameClear(){}
+    public void GameOver(){}
+    public void GamePause(){}
+
+}
