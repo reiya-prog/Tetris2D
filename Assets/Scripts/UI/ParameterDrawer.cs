b702e2c4f4a482208b5049c7fc4144fc0e567ffe
reiya-prog <t.reiya.2200@gmail.com>
2021-09-11 15:31:10 +0900

    各種UI表示用のスクリプトを作成。

---
+++ Assets/Scripts/UI/ParameterDrawer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+// Time, Level, Linesの描画を担当
+namespace Tetris
+{
+    namespace UI
+    {
+        public class ParameterDrawer : MonoBehaviour
+        {
+            // 編集するTextMeshProオブジェクト
+            // Timeを表示するテキスト
+            [SerializeField]
+            private TextMeshProUGUI _timeText;
+            // Levelを表示するテキスト
+            [SerializeField]
+            private TextMeshProUGUI _levelText;
+            // Linesを表示するテキスト
+            [SerializeField]
+            private TextMeshProUGUI _linesText;
+
+            // 出力するフォーマット
+            // timeのフォーマット。時:分:秒:ミリ秒で表示
+            private const string kTimeFormat = "{0:D2}:{1:D2}:{2:D2}:{3:D2}";
+            // levelのフォーマット。2桁0埋
+            private const string kLevelFormat = "{0:D2}";
+            // linesのフォーマット。4桁0埋
+            private const string kLinesFormat = "{0:D4}";
+
+            public void timeDraw(float deltaTimeFromStart)
+            {
+                _timeText.text = System.String.Format(kTimeFormat, deltaTimeFromStart);
+            }
+
+            public void levelDraw(int level)
+            {
+                _levelText.text = System.String.Format(kLevelFormat, level);
+            }
+
+            public void linesDraw(int lines)
+            {
+                _linesText.text = System.String.Format(kLinesFormat, lines);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TechnicalDrawer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Tetris
+{
+    namespace UI
+    {
+        public class TechnicalDrawer
+        {
+            // 編集するTextMeshProオブジェクト
+            // LENを表示するテキスト
+            [SerializeField]
+            private TextMeshProUGUI _lenText;
+            // TetrisとT-Spin等を表示するテキスト
+            [SerializeField]
+            private TextMeshProUGUI _tetrisAndSpinText;
+            // Back-to-Backを表示するテキスト
+            [SerializeField]
+            private TextMeshProUGUI _backToBackText;
+
+            // 出力するフォーマットとテキスト
+            // Lenのフォーマット。Len数+\n+LEN
+            private const string kLenFormat = "{0}\nLEN";
+            // Tetrisのテキスト。Tetris!と表示する。
+            private const string kTetrisText = "Tetris!";
+            // Tspinのテキスト。T SPIN!の表示に続けてSpinの詳細を表示する。
+            private const string kTspinText = "T SPIN!";
+            private const string kTspinMiniText = "Mini"; // Tspin Mini
+            private const string kTspinSingleText = "Single"; // Tspin Single
+            private const string kTspinDoubleText = "Double"; // Tspin Double
+            private const string kTspinTripleText = "Triple"; // Tspin Triple
+            // Back-to-Backのテキスト。Back-to-Backと表示する。
+            private const string kBackToBackText = "Back-to-Back";
+            public void LenDraw(int lenNum)
+            {
+                _lenText.text = System.String.Format(kLenFormat, lenNum);
+            }
+
+            public void TetrisDraw()
+            {
+                _tetrisAndSpinText.text = kTetrisText;
+            }
+
+            public void SpinDraw(int spinType)
+            {
+                _tetrisAndSpinText.text = kTspinText;
+                switch (spinType)
+                {
+                    case 0: // Tspin Mini
+                        _tetrisAndSpinText.text += kTspinMiniText;
+                        break;
+                    case 1: // Tspin Single
+                        _tetrisAndSpinText.text += kTspinSingleText;
+                        break;
+                    case 2: // Tspin Double
+                        _tetrisAndSpinText.text += kTspinDoubleText;
+                        break;
+                    case 3: // Tspin Triple
+                        _tetrisAndSpinText.text += kTspinTripleText;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            public void BackToBackDraw()
+            {
+                _backToBackText.text = kBackToBackText;
+            }
+        }
+    }
+}
